9c93dd27549b506c4ab8067f65a9bac5869842b7
peigo <candane@126.com>
2023-03-17 17:01:37 +0800

    config: rm pwa

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,14 +1,14 @@
 const plugins = [
-  [
-    '@vuepress/pwa',
-    {
-      serviceWorker: true,
-      updatePopup: {
-        message: '发现新内容可用',
-        buttonText: '刷新',
-      },
-    },
-  ],
+  // [
+  //   '@vuepress/pwa',
+  //   {
+  //     serviceWorker: true,
+  //     updatePopup: {
+  //       message: '发现新内容可用',
+  //       buttonText: '刷新',
+  //     },
+  //   },
+  // ],
   ['@vuepress-reco/extract-code'],
   [
     'vuepress-plugin-nuggets-style-copy',
